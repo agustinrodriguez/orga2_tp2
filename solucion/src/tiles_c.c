ee217c2a2eb8d8550e608a5adaa49473160ed06c
Agustin <agustin@agustin-K53SC.(none)>
2014-04-22 21:10:38 -0300

    Principio de implementacion de tiles.c

--- solucion/src/tiles_c.c
+++ solucion/src/tiles_c.c
@@ -1,6 +1,6 @@
 
 #include "tp2.h"
-
+#include <stdio.h>
 
 void tiles_c    (
 	unsigned char *src,
@@ -16,12 +16,38 @@ void tiles_c    (
 {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+	int cont_x = offsetx;
+	int cont_y = offsety;
 
-	for (int i_d = 0, i_s = 0; i_d < filas; i_d++, i_s++) {
+	/*for (int i_d = 0, i_s = 0; i_d < filas; i_d++, i_s++) {
 		for (int j_d = 0, j_s = 0; j_d < cols; j_d++, j_s++) {
 			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
 			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
 			*p_d = *p_s;
+		}
+	}*/
+
+	printf("Ancho a copiar (x) %d", tamx);
+	printf("\nAlto a copiar (y) %d", tamy);
+	printf("\nAncho que se saltea (x) %d", offsetx);
+	printf("\nAlto que se saltea (y) %d", offsety);
+
+	for (int i_d = 0; i_d	 < filas; i_d++) {
+		for (int j_d = 0; j_d < cols; j_d++) {
+			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
+			rgb_t *p_s = (rgb_t*)&src_matrix[cont_y][cont_x*3];
+			*p_d = *p_s;
+
+			printf("\nx: %d; y: %d", cont_x, cont_y);
+
+			cont_x++;
+			if (cont_x >= tamx) {
+				cont_x = offsetx;
+			}
+		}
+		cont_y++;
+		if (cont_y >= tamy) {
+			cont_y = offsety;
 		}
 	}
 
