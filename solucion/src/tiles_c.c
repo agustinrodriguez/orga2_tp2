53d6b81655bafd356d4bae07cb76e76e00ecf940
Agustin <agustin@agustin-K53SC.(none)>
2014-04-24 21:16:45 -0300

    Implementacion de tiles c corregida

--- solucion/src/tiles_c.c
+++ solucion/src/tiles_c.c
@@ -16,31 +16,25 @@ void tiles_c    (
 {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-	int cont_x = 0;
-	int cont_y = 0;
+	int f = offsety;
+	int c = offsetx;
 
-	cont_x = cols/tamx;
-	cont_y = filas/tamy;
-
-
-	for (int i_d = 0, i_s = offsetx; i_d < filas; i_d++,i_s++) {
-		for (int j_d = 0, j_s = offsety; j_d < cols; j_d++,j_s++) {
+	for (int i_d = 0; i_d < filas; i_d++) {
+		for (int j_d = 0; j_d < cols; j_d++) {
 			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
-			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
+			rgb_t *p_s = (rgb_t*)&src_matrix[f][c*3];
 			*p_d = *p_s;
 
-			if (j_s == cols) {
-				j_s = offsety;
+			c++;
+			if (c > (tamx + offsetx)) {
+				c = offsetx;
 			}
-			if (i_s == filas) {
-				i_s = offsetx;
-				cont_y--;
-				cont_x--;
-			}
-			if (cont_y == 0 && cont_x == 0) {
-				j_s = tamx;
-				i_s = tamy;
-			}
+		}
+		c = offsetx;
+
+		f++;
+		if (f > (tamy + offsety)) {
+			f = offsety;
 		}
 	}
 
