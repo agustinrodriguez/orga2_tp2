9bddbdd4a6d9accc9fd3cb2d8678429cce85b5c7
guidotripodi <guido.tripodi@hotmail.com>
2014-04-26 17:54:22 -0300

    sube guidin temperature_c

--- solucion/src/temperature_c.c
+++ solucion/src/temperature_c.c
@@ -19,12 +19,39 @@ void temperature_c    (
 {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-
+	int suma = 0;
+	double divido = 0;
 	for (int i_d = 0, i_s = 0; i_d < filas; i_d++, i_s++) {
 		for (int j_d = 0, j_s = 0; j_d < cols; j_d++, j_s++) {
 			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
-			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
-			*p_d = *p_s;
+			rgb_t *p_s = (rgb_t*)&src_matrix[i_d][j_d*3];
+			suma = p_s->r + p_s->b + p_s->g;
+			divido = (double)suma/3;
+			if (between(divido,0,31)){
+				p_d->r = 0;
+				p_d->g =0;
+				p_d->b = (128+ (4*divido));
+			}
+			if (between(divido,32,95)){
+				p_d->r = 0;
+				p_d->g =(divido-32)*4;
+				p_d->b = 255;
+			}	
+			if (between(divido,96,159)){
+				p_d->r = (divido-96)*4;
+				p_d->g =255;
+				p_d->b = 255-((divido-96)*4);
+			}
+			if (between(divido,160,223)){
+				p_d->r = 255;
+				p_d->g = 255 - ((divido-160)*4);
+				p_d->b = 0;
+			}
+			if (between(divido,224,255)){
+				p_d->r = 255-(divido-224);
+				p_d->g =0;
+				p_d->b = 0;
+			}
 		}
 	}
 }
