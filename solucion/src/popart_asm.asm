ddc018d7670d3f900e4c15a3b163b3cf0b97c7e5
Agustin Rodriguez <agusrodriguez@ws18.labo1>
2014-04-30 21:30:13 -0300

    sube guido popart

--- solucion/src/popart_asm.asm
+++ solucion/src/popart_asm.asm
@@ -3,8 +3,24 @@ global popart_asm
 %define pixels_por_ciclo 15
 
 section .rodata align = 16
-	MASK_1_COLOR: DB 0x80, 0x00, 0x80, 0x03, 0x80, 0x60, 0x80, 0x90, 0x80, 0x0C, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
+	MASK_1_COLOR: DB 0x00, 0x03, 0x06, 0x09, 0x0C, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
+	MASK_1_COLORGREEN: DB 0x01, 0x04,0x07, 0x0A, 0x0D, 0x80, 0x80, 0x80, 0x80, 0x80,0x80,0x80,0x80,0x80,0x80,0x80
+	MASK_1_COLORBLUE: DB 0x02,0x05, 0x08, 0x0B, 0x0E, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 , 0x80, 0x80, 0x80, 0x80, 0x80
 	UNO: DB 0x80, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80
+
+	;MASCARA PARA CASO 255
+	MASK_1: DB 0xFF,0x00,0x00,0xFF,0x00,0x00,0xFF,0x00,0x00,0xFF,0x00,0x00,0x00,0x00,0x00,0x00
+	MASK_2: DB 0x7F,0x00,0x7F,0x7F,0x00,0x7F,0x7F,0x00,0x7F,0x7F,0x00,0x7F,0x00,0x00,0x00,0x00
+	MASK_3: DB 0xFF,0x00,0xFF,0xFF,0x00,0xFF,0xFF,0x00,0xFF,0xFF,0x00,0xFF,0x00,0x00,0x00,0x00
+	MASK_4: DB 0x00,0x00,0xFF,0x00,0x00,0xFF,0x00,0x00,0xFF,0x00,0x00,0xFF,0x00,0x00,0x00,0x00
+	MASK_5: DB 0x00,0xFF,0xFF,0x00,0xFF,0xFF,0x00,0xFF,0xFF,0x00,0xFF,0xFF,0x00,0x00,0x00,0x00
+
+
+	MASK_152: DW 0x98,0x98,0x98,0x98,0x98,0x98,0x98,0x98
+	MASK_305: DW 0x131,0x131,0x131,0x131,0x131,0x131,0x131,0x131
+	MASK_459: DW 0x132,0x132,0x132,0x132,0x132,0x132,0x132,0x132
+	MASK_611: DW 0x263,0x263,0x263,0x263,0x263,0x263,0x263,0x263
+	MASK_764: DW 0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC
 
 section .data
 
@@ -43,29 +59,135 @@ popart_asm:
 		movdqu XMM0, [RDI] ; xmm0(B) = | b | g | r | b | g | r | b | g | r | b | g | r | b | g | r | b |
 
 		movdqu XMM1, XMM0 ; XMM1 = XMM0
-		movdqu XMM2, XMM0 ; XMM2 = XMM0
-		movdqu XMM3, XMM0 ; XMM3 = XMM0
-
-		pshufb XMM0, [MASK_1_COLOR] ; XMM0(B) = | 0 | b | 0 | b | 0 | b | 0 | b | 0 | b | 0 | 0 | 0 | 0 | 0 | 0 |
-
-		movdqu XMM15, [MASK_1_COLOR]
-		paddb XMM15, [UNO] ; configuro la mascara para que me queden los indices en el siguiente color
-		pshufb XMM1, XMM15 ; XMM1(B) = | 0 | g | 0 | g | 0 | g | 0 | g | 0 | g | 0 | 0 | 0 | 0 | 0 | 0 |
-
-		paddb XMM15, [UNO] ; configuro la mascara para que me queden los indices en el siguiente color
-		pshufb XMM2, XMM15 ; XMM2 = | 0 | r | 0 | r | 0 | r | 0 | r | 0 | r | 0 | 0 | 0 | 0 | 0 | 0 |
-
-		; sumos los words
-		paddw XMM0, XMM1 ; XMM0 = | b + g | b + g | b + g | b + g | b + g | 0 | 0 | 0 |
-		paddw XMM0, XMM2 ; XMM0 = | b + g + r | b + g + r | b + g + r | b + g + r | b + g + r | 0 | 0 | 0 |
+		movdqu xmm2,xmm0
+		;XORPD xmm7,xmm7
+		;punpcklbw xmm10, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		;punpckhbw xmm11, xmm7 ; xmm2 = 0ja15j : : : j0ja8
 
 
+		pshufb XMM0, [MASK_1_COLOR] ; RED
+		movdqu xmm10, xmm0
+		movdqu xmm11, xmm0
+		XORPD xmm7,xmm7
+		punpcklbw xmm10, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		punpckhbw xmm11, xmm7 ; xmm2 = 0ja15j : : : j0ja8
+
+		pshufb XMM1, [MASK_1_COLORGREEN] ; GREEN
+		movdqu XMM12, xmm1
+		movdqu XMM13, xmm1
+		XORPD xmm7,xmm7
+		punpcklbw xmm12, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		punpckhbw xmm13, xmm7 ; xmm2 = 0ja15j : : : j0ja8
+
+		pshufb XMM2, [MASK_1_COLORBLUE] ; BLUE
+		movdqu XMM14, xmm2
+		movdqu XMM15, xmm2
+		XORPD xmm7,xmm7
+		punpcklbw xmm14, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		punpckhbw xmm15, xmm7 ; xmm2 = 0ja15j : : : j0ja8
+		
+		paddw xmm10,xmm12
+		paddw xmm10,xmm14
+
+		paddw xmm11, xmm13
+		paddw xmm11,xmm15
+
+		paddd xmm10,xmm11
+		movdqu xmm0,xmm10
+		
+
+		XORPD XMM1,XMM1
+		XORPD XMM2, XMM2
+		XORPD XMM3,XMM3
+		XORPD XMM4,XMM4
+		XORPD XMM5,XMM5
+
+		XORPD XMM14,XMM14
+		movdqu xmm15, XMM0 ; lo salvo 
+		PCMPEQW XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR O IGUAL A CERO
+		jge .chequeo
+
+	.sigo:
+		ORPD XMM1,XMM2
+		ORPD XMM1, XMM3
+		ORPD XMM1,XMM4
+		ORPD XMM1,XMM5
+		movdqu xmm0, xmm1
 		movdqu [RSI], XMM0
-
 		add RDI, pixels_por_ciclo
 		add RSI, pixels_por_ciclo
 		sub R10D, pixels_por_ciclo
 		jmp .ciclo
+
+
+	.chequeo: 
+		
+		.sigoCon5:
+			movdqu XMM14,[MASK_611]
+			movdqu xmm15,xmm0
+			pcmpgtw XMM15, XMM14  ; COMPARO PACK A PACK SI ES MENOR O IGUAL A 152
+			jmp .elQuinto
+
+		.sigoCon4:
+			movdqu XMM14,[MASK_459]
+			movdqu xmm15,xmm0
+			pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MENOR O IGUAL A 152
+			jmp .elCuarto
+
+		.sigoCon3:
+			movdqu XMM14,[MASK_305]
+			movdqu xmm15,xmm0
+			pcmpgtw XMM15, XMM14  ; COMPARO PACK A PACK SI ES MENOR O IGUAL A 152
+			jmp .elTercero
+
+		.sigoCon2:
+			movdqu XMM14,[MASK_152]
+			movdqu xmm15,xmm0
+			pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MENOR O IGUAL A 152
+			jmp .elSegundo
+
+		.sigoCon1:
+			movdqa XMM14,[MASK_152]
+			movdqu xmm15,XMM0
+			pcmpgtw XMM14, XMM15 ; COMPARO PACK A PACK SI ES MENOR O IGUAL A 152
+			jmp .elPrimero
+	
+		
+	.elPrimero:
+		XORPD XMM13, XMM13
+		movdqu XMM13,[MASK_1]
+		andps xmm13, xmm14
+		ANDNPS xmm0,xmm14
+		movdqu xmm1, xmm13
+		jmp .sigo
+	
+	.elSegundo:
+		movdqu XMM13,[MASK_2]
+		pand xmm13, xmm15
+		ANDNPS xmm0,xmm15
+		movdqu xmm2, xmm13
+		jmp .sigoCon1
+
+	.elTercero:
+		movdqu XMM13,[MASK_3]
+		pand xmm13, xmm15
+		ANDNPS xmm0,xmm15
+		movdqu xmm3, xmm13
+		jmp .sigoCon2
+
+	.elCuarto:
+		movdqu XMM11,[MASK_4]
+		pand xmm13, xmm15
+		ANDNPS xmm0,xmm15
+		movdqu xmm4, xmm13
+		jmp .sigoCon3
+
+	.elQuinto:
+		movdqu XMM12,[MASK_5]
+		pand xmm13, xmm15
+		ANDNPS xmm0,xmm15
+		movdqu xmm5, xmm13
+		jmp .sigoCon4
 
 	.fin:
 	pop R13
