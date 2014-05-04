82a4bcf94922e00b1f99d09336b391c7fda28452
Agustin <agustin@agustin-K53SC.(none)>
2014-05-04 11:40:15 -0300

    Mejorado ldr c

--- solucion/src/ldr_c.c
+++ solucion/src/ldr_c.c
@@ -13,54 +13,41 @@ void ldr_c    (
     int filas,
     int src_row_size,
     int dst_row_size,
-	int alfa)
+    int alfa)
 {
     unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
     int sumar,sumag,sumab = 0;
-    int max = 5*5*255*3*255;
+    int max = 4876875; // 5*5*255*3*255;
     int sumargb = 0;
     int multiplico = 1;
-    double var = 0;
+    double varr = 0;
+    double varg = 0;
+    double varb = 0;
     int srcsuma = 0;
-    for (int i = 0; i < filas; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < cols; j++) {
             rgb_t *p_d = (rgb_t*) &dst_matrix[i][j * 3];
             rgb_t *p_s = (rgb_t*) &src_matrix[i][j * 3];
-            if (i < 2 || j < 2 || (i + 2) == filas || (j + 2) == cols){
-                    *p_d=*p_s;
-                }
-            else{
-                sumar = 0;
-                sumag = 0;
-                sumab = 0;
+            if (i < 2 || j < 2 || (i + 2) == filas || (j + 2) == cols) {
+                *p_d=*p_s;
+            } else {
+                sumargb = 0;
                 //SI FUNCIONA ESTO ARMAR UNA FUNCION QUE HAGA ESTO PARA SIMPLIFICAR
-                    for (int i_p = i-2; i_p <= i+2; ++i_p){
-                        for (int j_p = j-2; j_p <= j+2; ++j_p){
-                            rgb_t *s_s = (rgb_t*) &src_matrix[i_p][j_p*3];
-                            sumar = sumar + s_s->r;
-                        }
+                for (int i_p = i-2; i_p <= i+2; ++i_p) {
+                    for (int j_p = j-2; j_p <= j+2; ++j_p) {
+                        rgb_t *s_s = (rgb_t*) &src_matrix[i_p][j_p*3];
+                        sumargb += s_s->r + s_s->g + s_s->b;
                     }
-                         for (int i_p = i-2; i_p <= i+2; ++i_p){
-                        for (int j_p = j-2; j_p <= j+2; ++j_p){
-                            rgb_t *s_s = (rgb_t*) &src_matrix[i_p][j_p*3];
-                            sumag = sumag + s_s->g;
-                        }
-                    }
-                         for (int i_p = i-2; i_p <= i+2; ++i_p){
-                        for (int j_p = j-2; j_p <= j+2; ++j_p){
-                            rgb_t *s_s = (rgb_t*) &src_matrix[i_p][j_p*3];
-                            sumab = sumab + s_s->b;
-                        }
-                    }
-                    sumargb = sumar + sumab + sumag;
-                    multiplico = sumargb*srcsuma*alfa;
-                    var= (double)multiplico/max;
-                    p_d->r= MIN(MAX(( p_s->r + var),0),255);
-                    p_d->b= MIN(MAX(( p_s->b + var),0),255);
-                    p_d->g= MIN(MAX(( p_s->g + var),0),255);
+                }
+
+                varr = (double) (p_s->r * alfa * sumargb/max);
+                varg = (double) (p_s->g * alfa * sumargb/max);
+                varb = (double) (p_s->b * alfa * sumargb/max);
+
+                p_d->r= MIN(MAX(( p_s->r + varr),0),255);
+                p_d->b= MIN(MAX(( p_s->b + varg),0),255);
+                p_d->g= MIN(MAX(( p_s->g + varb),0),255);
             }
         }
     }
