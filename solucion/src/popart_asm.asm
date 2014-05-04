e368e40ce9438d50004a7e3fba25b7eaa2ec8fbb
Agustin <agustin@agustin-K53SC.(none)>
2014-05-03 23:43:43 -0300

    Mejoras en el codigo popart

--- solucion/src/popart_asm.asm
+++ solucion/src/popart_asm.asm
@@ -18,11 +18,11 @@ section .rodata align = 16
 	MASK_DE1WORDA3BYTES: DB 0x00,0x00,0x00,0x02,0x02,0x02,0x04,0x04,0x04,0x06,0x06,0x06,0x08,0x08,0x08,0x80
 
 
-	MASK_152: DW 0x98,0x98,0x98,0x98,0x98,0x80,0x80,0x80
-	MASK_305: DW 0x131,0x131,0x131,0x131,0x131,0x131,0x131,0x131
-	MASK_459: DW 0x1CB,0x1CB,0x1CB,0x1CB,0x1CB,0x1CB,0x1CB,0x1CB
-	MASK_611: DW 0x263,0x263,0x263,0x263,0x263,0x263,0x263,0x263
-	MASK_764: DW 0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC,0x2FC
+	MASK_152: DW 0x0098,0x0098,0x0098,0x0098,0x0098,0x0080,0x0080,0x0080
+	MASK_153: DW 0x0099,0x0099,0x0099,0x0099,0x0099,0x0080,0x0080,0x0080
+	MASK_305: DW 0x0131,0x0131,0x0131,0x0131,0x0131,0x0131,0x0131,0x0131
+	MASK_458: DW 0x01CA,0x01CA,0x01CA,0x01CA,0x01CA,0x01CA,0x01CA,0x01CA
+	MASK_611: DW 0x0263,0x0263,0x0263,0x0263,0x0263,0x0263,0x0263,0x0263
 
 section .data
 
@@ -58,50 +58,46 @@ popart_asm:
 	.ciclo:
 		cmp R10D, 0 ; si recorri todos los bytes
 		jle .fin
-		movdqu XMM0, [RDI] ; xmm0(B) = | b | g | r | b | g | r | b | g | r | b | g | r | b | g | r | b |
+		movdqu XMM0, [RDI] ; XMM0(B) = |b0|g0|r0|b1|g1|r1|b2|g2|r2|b3|g3|r3|b4|g4|r4|b|
 
 		movdqu XMM1, XMM0 ; XMM1 = XMM0
-		movdqu xmm2,xmm0
+		movdqu XMM2, XMM0 ; XMM2 = XMM0
 		
-		pshufb XMM0, [MASK_1_COLOR] ; RED
-		movdqu xmm10, xmm0
-		XORPD xmm7,xmm7
-		punpcklbw xmm10, xmm7 ; xmm1 = 0ja7j : : : j0ja0
-		
-		pshufb XMM1, [MASK_1_COLORGREEN] ; GREEN
-		movdqu XMM12, xmm1
-		XORPD xmm7,xmm7
-		punpcklbw xmm12, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		XORPD XMM7, XMM7 ; XMM7 = 0
 
-		pshufb XMM2, [MASK_1_COLORBLUE] ; BLUE
-		movdqu XMM14, xmm2
-		XORPD xmm7,xmm7
-		punpcklbw xmm14, xmm7 ; xmm1 = 0ja7j : : : j0ja0
+		pshufb XMM0, [MASK_1_COLOR] ; BLUE XMM0 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
+		movdqu XMM10, XMM0
+		punpcklbw XMM10, XMM7 ; XMM10 = |0 b0|0 b1|0 b2|0 b3|0 b4|
+		
+		pshufb XMM1, [MASK_1_COLORGREEN] ; GREEN XMM1 = |g0|g1|g2|g3|g4|0|0|0|0|0|0|0|0|0|0|0|
+		movdqu XMM12, XMM1
+		punpcklbw XMM12, XMM7 ; XMM12 = |0 g0|0 g1|0 g2|0 g3|0 g4|
+
+		pshufb XMM2, [MASK_1_COLORBLUE] ; RED XMM2 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
+		movdqu XMM14, XMM2
+		punpcklbw XMM14, XMM7 ; XMM14 = |0 r0|0 r1|0 r2|0 r3|0 r4|
 
 		
-		paddw xmm10,xmm12
-		paddw xmm10,xmm14
-		movdqu xmm0, xmm10
+		paddw XMM10, XMM12
+		paddw XMM10, XMM14
+		movdqu XMM0, XMM10 ; XMM0 = |b0 + g0 + r0|b1 + g1 + r1|b2 + g2 + r2|b3 + g3 + r3|b4 + g4 + r4|
 		
-		XORPD XMM1,XMM1
+		XORPD XMM1, XMM1
 		XORPD XMM2, XMM2
-		XORPD XMM3,XMM3
-		XORPD XMM4,XMM4
-		XORPD XMM5,XMM5
-		XORPD xmm10, xmm10
+		XORPD XMM3, XMM3
+		XORPD XMM4, XMM4
+		XORPD XMM5, XMM5
+		XORPD XMM10, XMM10
 
-		XORPD XMM14,XMM14
-		movdqu xmm15, XMM0 ; lo salvo 
-		PCMPEQW XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR O IGUAL A CERO
-		jge .chequeo
+		jmp .chequeo
 
 	.sigo:
-		por XMM1,XMM2
-		por XMM1,XMM3
-		por XMM1,XMM4
-		por XMM1,XMM5
-		movdqu xmm0, xmm1
-		;pshufb xmm0, [MASK_FIN] no se si hay q guardarlo al reves o no
+		por XMM1, XMM2
+		por XMM1, XMM3
+		por XMM1, XMM4
+		por XMM1, XMM5
+		movdqu XMM0, XMM1
+		;pshufb XMM0, [MASK_FIN] no se si hay q guardarlo al reves o no
 		movdqu [RSI], XMM0
 		add RDI, pixels_por_ciclo
 		add RSI, pixels_por_ciclo
@@ -112,49 +108,49 @@ popart_asm:
 		
 		.sigoCon5:
 			movdqu XMM14,[MASK_611]
-			movdqu xmm15,xmm0
+			movdqu XMM15, XMM0
 			pcmpgtw XMM15, XMM14  ; COMPARO PACK A PACK SI ES MAYOR A 611
 			jmp .elQuinto
 
 		.sigoCon4:
-			movdqu XMM14,[MASK_459]
-			movdqu xmm15,xmm0
+			movdqu XMM14,[MASK_458]
+			movdqu XMM15, XMM0
 			pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 459
 			jmp .elCuarto
 
 		.sigoCon3:
 			movdqu XMM14,[MASK_305]
-			movdqu xmm15,xmm0
+			movdqu XMM15, XMM0
 			pcmpgtw XMM15, XMM14  ; COMPARO PACK A PACK SI ES MAYOR A 305
 			jmp .elTercero
 
 		.sigoCon2:
 			movdqu XMM14,[MASK_152]
-			movdqu xmm15,xmm0
+			movdqu XMM15, XMM0
 			pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 152
 			jmp .elSegundo
 
 		.sigoCon1:
-			movdqa XMM14,[MASK_152]
-			movdqu XMM6, XMM10 ; me quedo con xmm10 y laburo con el de xmm6
-			pcmpeqw XMM7,XMM7
-			pxor xmm6, xmm7
-			pand xmm14,xmm6
-			movdqu xmm15,XMM0
-			pcmpgtw XMM14, XMM15 ; COMPARO PACK A PACK SI ES MENOR A 152
+			movdqa XMM14,[MASK_153]
+			movdqu XMM6, XMM10 ; me quedo con XMM10 y laburo con el de XMM6
+			pcmpeqw XMM7, XMM7 ; XMM7 = |1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1
+			pxor XMM6, XMM7 ; deja en 1 este caso y 0 los casos ya checkeados
+			pand XMM14, XMM6 ; en XMM14 queda los 153 en los lugares que tengo que comparar
+			movdqu XMM15, XMM0
+			pcmpgtw XMM14, XMM15 ; COMPARO PACK A PACK SI ES MENOR A 153
 			jmp .elPrimero
 	
 		
 	.elPrimero:
 		movdqu XMM13,[MASK_1]
-		movdqu xmm11, xmm14
+		movdqu XMM11, XMM14
 		PADDUSW XMM10, XMM14  ;este lo uso para poner en 1 los pack que ya tuvieron su caso
 		pcmpeqw XMM15,XMM15
-		pxor xmm11, xmm15
-		pand xmm0,xmm11
+		pxor XMM11, XMM15
+		pand XMM0, XMM11
 		pshufb XMM14, [MASK_DE1WORDA3BYTES]
-		pand xmm13, xmm14
-		movdqu xmm1, xmm13
+		pand XMM13, XMM14 ; en XMM13 queda los colores a poner en el lugar que van
+		movdqu XMM1, XMM13
 		jmp .sigo
 	
 	.elSegundo:
@@ -162,48 +158,48 @@ popart_asm:
 		movdqu xmm11, xmm15
 		PADDUSW XMM10, XMM15 ;este lo uso para poner en 1 los pack que ya tuvieron su caso
 		pcmpeqw XMM14,XMM14
-		pxor xmm11, xmm14
-		pand xmm0,xmm11
+		pxor XMM11, XMM14
+		pand XMM0, XMM11
 		pshufb XMM15, [MASK_DE1WORDA3BYTES]
-		pand xmm13, xmm15
-		movdqu xmm2, xmm13
+		pand XMM13, XMM15
+		movdqu XMM2, XMM13
 		jmp .sigoCon1
 
 	.elTercero:
-		movdqu XMM13,[MASK_3]
-		movdqu xmm11, xmm15
+		movdqu XMM13, [MASK_3]
+		movdqu XMM11, XMM15
 		PADDUSW XMM10, XMM15 ;este lo uso para poner en 1 los pack que ya tuvieron su caso
-		pcmpeqw XMM14,XMM14 
-		pxor xmm11, xmm14
-		pand xmm0,xmm11
+		pcmpeqw XMM14, XMM14 
+		pxor XMM11, XMM14
+		pand XMM0, XMM11
 
 		pshufb XMM15, [MASK_DE1WORDA3BYTES]
-		pand xmm13, xmm15
-		movdqu xmm3, xmm13
+		pand XMM13, XMM15
+		movdqu XMM3, XMM13
 		jmp .sigoCon2
 
 	.elCuarto:
-		movdqu XMM13,[MASK_4]
+		movdqu XMM13, [MASK_4]
 		movdqu xmm11, xmm15		
 		PADDUSW XMM10, XMM15 ;este lo uso para poner en 1 los pack que ya tuvieron su caso
-		pcmpeqw XMM14,XMM14
-		pxor xmm11, xmm14
-		pand xmm0,xmm11
+		pcmpeqw XMM14, XMM14
+		pxor XMM11, XMM14
+		pand XMM0, XMM11
 		pshufb XMM15, [MASK_DE1WORDA3BYTES]
-		pand xmm13, xmm15
-		movdqu xmm4, xmm13
+		pand XMM13, XMM15
+		movdqu XMM4, XMM13
 		jmp .sigoCon3
 
 	.elQuinto:
-		movdqu XMM13,[MASK_5]
+		movdqu XMM13, [MASK_5]
 		PADDUSW XMM10, XMM15 ;este lo uso para poner en 1 los pack que ya tuvieron su caso
-		movdqu xmm11, xmm15
-		pcmpeqw XMM14,XMM14
-		pxor xmm11, xmm14
-		pand xmm0,xmm11
+		movdqu XMM11, XMM15
+		pcmpeqw XMM14, XMM14
+		pxor XMM11, XMM14
+		pand XMM0, XMM11
 		pshufb XMM15, [MASK_DE1WORDA3BYTES]
-		pand xmm13, xmm15
-		movdqu xmm5, xmm13
+		pand XMM13, XMM15
+		movdqu XMM5, XMM13
 		jmp .sigoCon4
 
 	.fin:
