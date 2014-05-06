1943e541f3358aa93c5658e6fbff5366cb094c37
Agustin <agustin@agustin-K53SC.(none)>
2014-05-06 01:21:15 -0300

    LDR.c mejorado

--- solucion/src/ldr_c.c
+++ solucion/src/ldr_c.c
@@ -19,21 +19,18 @@ void ldr_c    (
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
     int sumar,sumag,sumab = 0;
     int max = 4876875; // 5*5*255*3*255;
-    int sumargb = 0;
-    int multiplico = 1;
+    double sumargb = 0;
     double varr = 0;
     double varg = 0;
     double varb = 0;
-    int srcsuma = 0;
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < cols; j++) {
             rgb_t *p_d = (rgb_t*) &dst_matrix[i][j * 3];
             rgb_t *p_s = (rgb_t*) &src_matrix[i][j * 3];
-            if (i < 2 || j < 2 || (i + 2) == filas || (j + 2) == cols) {
+            if (i < 2 || j < 2 || (i + 2) >= filas || (j + 2) >= cols) {
                 *p_d=*p_s;
             } else {
                 sumargb = 0;
-                //SI FUNCIONA ESTO ARMAR UNA FUNCION QUE HAGA ESTO PARA SIMPLIFICAR
                 for (int i_p = i-2; i_p <= i+2; ++i_p) {
                     for (int j_p = j-2; j_p <= j+2; ++j_p) {
                         rgb_t *s_s = (rgb_t*) &src_matrix[i_p][j_p*3];
@@ -41,13 +38,17 @@ void ldr_c    (
                     }
                 }
 
-                varr = (double) (p_s->r * alfa * sumargb/max);
-                varg = (double) (p_s->g * alfa * sumargb/max);
-                varb = (double) (p_s->b * alfa * sumargb/max);
+                sumargb = sumargb/max;
 
-                p_d->r= MIN(MAX(( p_s->r + varr),0),255);
+                varr = (double) (p_s->r * alfa * sumargb);
+                varg = (double) (p_s->g * alfa * sumargb);
+                varb = (double) (p_s->b * alfa * sumargb);
+
                 p_d->b= MIN(MAX(( p_s->b + varg),0),255);
                 p_d->g= MIN(MAX(( p_s->g + varb),0),255);
+                p_d->r= MIN(MAX(( p_s->r + varr),0),255);
+
+                int k = 0;
             }
         }
     }
