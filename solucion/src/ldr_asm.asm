e4a937ac809b9f09ee1cde5e30ccf6b8502edc54
Agustin Rodriguez <agusrodriguez@ws19.labo2>
2014-05-06 00:00:53 -0300

    subiendo guido ldr

--- solucion/src/ldr_asm.asm
+++ solucion/src/ldr_asm.asm
@@ -9,16 +9,18 @@ section .rodata align = 16
 	MASK_1_COLORGREEN: DB 0x01, 0x04,0x07, 0x0A, 0x0D, 0x80, 0x80, 0x80, 0x80, 0x80,0x80,0x80,0x80,0x80,0x80,0x80
 	MASK_1_COLORBLUE: DB 0x02,0x05, 0x08, 0x0B, 0x0E, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 , 0x80, 0x80, 0x80, 0x80, 0x80
 	MASKPARACASOSPOSITIVOS: DB 0x00,0x02,0x04,0x06,0x08,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
-	MASK_FIN: DB 0x02,0x01,0x00,0x05,0x04,0x03,0x08,0x07,0x06,0x0B,0x0A,0x09,0x0E,0x0D,0x0C,0x80
-	MASKCOLORROJO: DB 0x00,0x80,0x80,0x01,0x80,0x80,0x02,0x80,0x80,0x03,0x80,0x80,0x04,0x80,0x80,0x80
-	MASKCOLORVERDE: DB 0x80,0x00,0x80,0x80,0x01,0x80,0x80,0x02,0x80,0x80,0x03,0x80,0x80,0x04,0x80,0x80
-	MASKCOLORAZUL: DB 0x80,0x80,0x00,0x80,0x80,0x01,0x80,0x80,0x03,0x80,0x80,0x04,0x80,0x80,0x05,0x80
+	MASK_FIN: DB 0x02,0x01,0x00,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
+	MASKCOLORROJO: DB 0x00,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
+	MASKCOLORVERDE: DB 0x80,0x00,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
+	MASKCOLORAZUL: DB 0x80,0x80,0x00,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
 	MALFA: DB 0x00,0x80,0x80,0x80,0x00,0x80,0x80,0x80,0x00,0x80,0x80,0x80,0x00,0x80,0x80,0x80
+	MEQUEDOCONELDELMEDIO: DB 0x06,0x07,0x08,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80,0x80
 ;mascaras en DW
 	M255: DW 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
 	M0: DW 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 ;mascaras en DD
 	MASK_MAX: DD 0x4A6A4B, 0x4A6A4B, 0x4A6A4B, 0x4A6A4B
+	prueba: DW 0x00+0x01, 0x80,0x80,0x80,0x80,0x80,0x80,0x80
 	
 	
 
@@ -118,8 +120,31 @@ ldr_asm:
 				paddw xmm3, xmm1
 				paddw xmm3, xmm4
 				paddw xmm3, XMM5
+				;TODOSPARAUNO
+				movdqu xmm8, xmm3
+				movdqu xmm9, xmm3   ;sumo primero con el ultimo
+				PSRLDQ XMM9, 8d
+				paddw xmm3, xmm9
+
+				movdqu xmm9, xmm8 ;sumo lo q ya tengo mas el penultimo
+				PSRLDQ XMM9, 6d
+				paddw xmm3, xmm9
+				
+				movdqu xmm9, xmm8 ;sumo lo q ya tengo mas el antpeneultimo
+				PSRLDQ XMM9, 4d
+				paddw xmm3, xmm9
+
+				movdqu xmm9, xmm8 ;sumo lo q ya tengo mas el q me falta
+				PSRLDQ XMM9, 2d
+				paddw xmm3, xmm9
+				
+				movdqu xmm9, xmm3
+				PSRLDQ XMM9, 2d
+				PSLLDQ XMM9, 2d
+				psubw xmm3, xmm9 ;resto para q no me quede basura
+
+				
 				;convierto a dw y a float para division
-				;vamos a tener q cambiar de float a double porq como el numero es gigantesco pone cero de una
 				; voy a multiplicar ahora por alfa
 				;convierto a float para multiplicar
 				movdqu xmm15, [RSP + 48]
@@ -131,123 +156,81 @@ ldr_asm:
 			.continuamos:
 				;en xmm6 me habia guardado los colores de cada pixel "actuales"
 				movdqu xmm0, xmm6
+				pshufb xmm0, [MEQUEDOCONELDELMEDIO]
 				jmp	.multiplicoColoresConAlfaYSumaRGBSobreMax
+			
 			.dividirPorMax:
-				;tengo en xmm1 b b b b b
-				;tengo en xmm2 g g g g g
-				;tengo en xmm4 r r r r r
-;ACA HAY Q CHEQUEAR
+
 				movdqu xmm11, [MASK_MAX]
 				cvtdq2pd xmm11,xmm11
-				; movdqu xmm7, xmm3
-				; movdqu xmm8, xmm3
-				; XORPD xmm9, xmm9
-				; punpcklwd xmm7, xmm9 ;parte baja
-				; punpckhwd xmm8, xmm9 ;parte alta
-				;blue
-				movdqu XMM9, XMM1
 				CVTPS2PD XMM1, XMM1
-				PSRLDQ XMM9, 8d
-				CVTPS2PD XMM9, XMM9
-				CVTPS2PD XMM2, XMM2
 				divpd xmm1,XMM11 
-				divpd xmm9,xmm11
-				divpd xmm2,xmm11
-
 				CVTPD2DQ xmm1,xmm1
-				CVTPD2DQ xmm9, xmm9
-				CVTPD2DQ xmm2, xmm2
-				PSLLDQ XMM9, 8d
-				paddd xmm1,xmm9
-				PACKUSDW xmm1, xmm2
+				PACKUSDW xmm1, xmm1
 			
 				;GREEN
-				movdqu XMM9, XMM5
 				CVTPS2PD XMM5, XMM5
-				PSRLDQ XMM9, 8d
-				CVTPS2PD XMM9, XMM9
-				CVTPS2PD XMM6, XMM6
 				divpd xmm5,XMM11 
-				divpd xmm9,xmm11
-				divpd xmm6,xmm11
-
 				CVTPD2DQ xmm5,xmm5
-				CVTPD2DQ xmm9, xmm9
-				CVTPD2DQ xmm6, xmm6
-				PSLLDQ XMM9, 8d
-				paddd xmm5,xmm9
-				PACKUSDW xmm5, xmm6
+				PACKUSDW xmm5, xmm5
 
 				;rojo
-				movdqu XMM9, XMM7
 				CVTPS2PD XMM7, XMM7
-				PSRLDQ XMM9, 8d
-				CVTPS2PD XMM9, XMM9
-				CVTPS2PD XMM8, XMM8
 				divpd xmm7,XMM11 
-				divpd xmm9,xmm11
-				divpd xmm8,xmm11
-
 				CVTPD2DQ xmm7,xmm7
-				CVTPD2DQ xmm9, xmm9
-				CVTPD2DQ xmm8, xmm8
-				PSLLDQ XMM9, 8d
-				paddd xmm7,xmm9
-				PACKUSDW xmm7, xmm8
+				PACKUSDW xmm7, xmm7
+
+
+				movdqu xmm9, xmm1
+				PSRLDQ XMM9, 2d
+				PSLLDQ XMM9, 2d
+				psubw xmm1, xmm9 ;resto para q no me quede basura
+
+				movdqu xmm9, xmm5
+				PSRLDQ XMM9, 2d
+				PSLLDQ XMM9, 2d
+				psubw xmm5, xmm9 ;resto para q no me quede basura
+
+				movdqu xmm9, xmm7
+				PSRLDQ XMM9, 2d
+				PSLLDQ XMM9, 2d
+				psubw xmm7, xmm9 ;resto para q no me quede basura
 
 				;en xmm1 tengo blue
 				;en xmm5 tengo GREEN
 				;en xmm7 tengo rojo
-				movdqu xmm2, xmm15
-				movdqu xmm6, xmm15
+				; en xmm6 me quede con el actual
+				movdqu xmm15, xmm6
+				pshufb xmm15, [MEQUEDOCONELDELMEDIO]
 				movdqu xmm8, xmm15
+				movdqu xmm9, xmm15
+				movdqu xmm10, xmm15
+				pshufb xmm8, [MASKCOLORROJO] ;dice rojo pero seria el blue
+				paddw xmm8, xmm1
+				pshufb xmm9, [MASKCOLORVERDE]
+				paddw xmm9, xmm5
+				pshufb xmm10, [MASKCOLORAZUL]
+				paddw xmm10, xmm7
 
-				pshufb XMM2, [MASK_1_COLOR] ; BLUE XMM0 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
-				movdqu xmm10, XMM2
-				punpcklbw XMM10, XMM9 ; XMM10 = |0 b0|0 b1|0 b2|0 b3|0 b4|
-				
-				pshufb XMM7, [MASK_1_COLORGREEN] ; GREEN XMM1 = |g0|g1|g2|g3|g4|0|0|0|0|0|0|0|0|0|0|0|
-				movdqu XMM12, XMM7
-				punpcklbw XMM12, XMM9 ; XMM12 = |0 g0|0 g1|0 g2|0 g3|0 g4|
-
-				pshufb XMM8, [MASK_1_COLORBLUE] ; RED XMM2 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
-				movdqu XMM14, XMM8
-				punpcklbw XMM14, XMM9 ; XMM14 = |0 r0|0 r1|0 r2|0 r3|0 r4|
-
-
-
-
-
-
-
-
-
-
-
-
-				movdqu xmm3, xmm0
-				paddw xmm1, xmm3
-				paddw xmm2, xmm3
-				paddw xmm4, xmm3
-				movdqu xmm0, xmm1
+				movdqu xmm0, xmm8
 				xor r15, r15
 				jmp .chequeo
 			.verdes:
-				movdqu xmm1, xmm0
-				movdqu xmm0, xmm2
+				movdqu xmm8, xmm0
+				movdqu xmm0, xmm9
 				jmp .chequeo
 			.rojos:
-				movdqu xmm2, xmm0
-				movdqu xmm0, xmm4
+				movdqu xmm9, xmm0
+				movdqu xmm0, xmm10
 				jmp .chequeo
 			.juntoTodo:
-				movdqu xmm4, xmm0
-				pshufb xmm1, [MASKCOLORROJO]
-				pshufb xmm2, [MASKCOLORVERDE]
-				pshufb xmm4, [MASKCOLORAZUL]
-				paddb xmm1, xmm2
-				paddb xmm1, xmm4
-				movdqu xmm0, xmm1
+				movdqu xmm10, xmm0
+				pshufb xmm8, [MASKCOLORROJO]
+				pshufb xmm9, [MASKCOLORVERDE]
+				pshufb xmm10, [MASKCOLORAZUL]
+				paddb xmm8, xmm9
+				paddb xmm8, xmm10
+				movdqu xmm0, xmm8
 				pshufb xmm0, [MASK_FIN]
 				jmp .finCiclo
 
@@ -311,96 +294,64 @@ ldr_asm:
 
 	.multiplicarXAlfa:
 		movdqu xmm7, xmm3
-		movdqu xmm8, xmm3
 		XORPD xmm9, xmm9
 		punpcklwd xmm7, xmm9
-		punpckhwd xmm8, xmm9
 		cvtdq2ps xmm7, xmm7
-		cvtdq2ps xmm8, xmm8
 		cvtdq2ps xmm15, xmm15
 
 		mulps XMM7, xmm15
-		mulps XMM8, xmm15
-
+		
 		CVTPS2DQ xmm7,xmm7
-		CVTPS2DQ xmm8, xmm8
 		movdqu xmm3, xmm7
-		movdqu xmm4, xmm8
 		jmp .continuamos
 
 		.multiplicoColoresConAlfaYSumaRGBSobreMax:
 			movdqu XMM7, XMM0 ; XMM1 = XMM0
 			movdqu XMM8, XMM0 ; XMM2 = XMM0
-			movdqu xmm15, xmm0 ; aca tengo los colores por separado sin ninguna operacion
-
+			
+			cvtdq2ps xmm3, xmm3
 
 			XORPD XMM9, XMM9 ; XMM7 = 0
 			;multiplico blue 
 			pshufb XMM0, [MASK_1_COLOR] ; BLUE XMM0 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
 			movdqu XMM10, XMM0
 			punpcklbw XMM10, XMM9 ; XMM10 = |0 b0|0 b1|0 b2|0 b3|0 b4|
-			;movdqu xmm1, xmm10
+			
 
 			movdqu xmm13, XMM10
-			movdqu xmm14, XMM10
 			XORPD xmm9, xmm9
 			punpcklwd xmm13, xmm9
-			punpckhwd xmm14, xmm9
 			cvtdq2ps xmm13, xmm13
-			cvtdq2ps xmm14, xmm14
-			cvtdq2ps xmm3, xmm3
-			cvtdq2ps xmm4, xmm4
-
+					
 			mulps xmm13, xmm3
-			mulps XMM14, xmm4
 			movdqu XMM1, XMM13
-			movdqu XMM2, XMM14
 
 			;multiplico verde
 
 			pshufb XMM7, [MASK_1_COLORGREEN] ; GREEN XMM1 = |g0|g1|g2|g3|g4|0|0|0|0|0|0|0|0|0|0|0|
 			movdqu XMM11, XMM7
 			punpcklbw XMM11, XMM9 ; XMM12 = |0 g0|0 g1|0 g2|0 g3|0 g4|
-			movdqu xmm2, xmm11
-
+			
 			movdqu xmm13, XMM11
-			movdqu xmm14, XMM11
 			XORPD xmm9, xmm9 
 			punpcklwd xmm13, xmm9
-			punpckhwd xmm14, xmm9
 			cvtdq2ps xmm13, xmm13
-			cvtdq2ps xmm14, xmm14
-			cvtdq2ps xmm3, xmm3
-			cvtdq2ps xmm4, xmm4
 
 			mulps xmm13, xmm3
-			mulps XMM14, xmm4
+
 			movdqu XMM5, XMM13
-			movdqu XMM6, XMM14
 
 			;multiplico red
 			pshufb XMM8, [MASK_1_COLORBLUE] ; RED XMM2 = |b0|b1|b2|b3|b4|0|0|0|0|0|0|0|0|0|0|0|
 			movdqu XMM12, XMM8
 			punpcklbw XMM12, XMM9 ; XMM14 = |0 r0|0 r1|0 r2|0 r3|0 r4|
-			movdqu xmm4, xmm12
-		
 			movdqu xmm13, XMM12
-			movdqu xmm14, XMM12
+
 			XORPD xmm9, xmm9
 			punpcklwd xmm13, xmm9
-			punpckhwd xmm14, xmm9
 			cvtdq2ps xmm13, xmm13
-			cvtdq2ps xmm14, xmm14
-			cvtdq2ps xmm3, xmm3
-			cvtdq2ps xmm4, xmm4
-
 			mulps xmm13, xmm3
-			mulps XMM14, xmm4
 			movdqu XMM7, XMM13
-			movdqu XMM8, XMM14
-
-
-
 			; paddd XMM10, XMM11
 			; paddd XMM10, XMM12
 			; movdqu XMM0, XMM10 ; XMM0 = |p_s->b * alfa * sumargb/max|p_s->g * alfa * sumargb/max|b2 + g2 + r2|b3 + g3 + r3|b4 + g4 + r4|
@@ -410,13 +361,14 @@ ldr_asm:
 			.chequeo:
 		movdqu XMM14,[M0]
 		movdqu xmm15,xmm0
-		pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 31
+		pcmpgtw XMM15, XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 0
 		jmp .elNumeroEsMayorACero
 
 	.chequeoLosMenoresACero:
 		movdqu XMM14,[M0]
+		pand xmm14,xmm11
 		movdqu xmm15,xmm0
-		pcmpgtw XMM14,XMM15 ; COMPARO PACK A PACK SI ES MAYOR A 31
+		pcmpgtw XMM14,XMM15 ; COMPARO PACK A PACK SI ES menor A 0
 		jmp .elNumeroNoEsMayorACero
 
 	.chequeoLosMenoresA255:
@@ -425,13 +377,13 @@ ldr_asm:
 		orpd xmm0,xmm2
 		movdqu XMM14,[M255]
 		movdqu xmm15,xmm0
-		pcmpgtw XMM14,XMM15 ; COMPARO PACK A PACK SI ES MAYOR A 31
+		pcmpgtw XMM14,XMM15 ; COMPARO PACK A PACK SI ES menor A 255
 		jmp .elNumeroEsMenorA255
 	
 	.chequeoLosMayoresA255:
 		movdqu XMM14,[M255]
 		movdqu xmm15,xmm0
-		pcmpgtw XMM15,XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 31
+		pcmpgtw XMM15,XMM14 ; COMPARO PACK A PACK SI ES MAYOR A 255
 		jmp .elNumeroEsMayorA255
 
 
@@ -444,8 +396,8 @@ ldr_asm:
 			pand xmm0,xmm11
 			pshufb XMM15, [MASKPARACASOSPOSITIVOS]
 			pand xmm13, xmm15
-			movdqu xmm2, xmm13
-			jmp .chequeoLosMenoresA255
+			movdqu xmm1, xmm13
+			jmp .chequeoLosMenoresACero
 
 		.elNumeroNoEsMayorACero:
 			movdqu XMM13, [M0]
@@ -462,7 +414,7 @@ ldr_asm:
 			pand xmm0,xmm11
 			pshufb XMM15, [MASKPARACASOSPOSITIVOS]
 			pand xmm13, xmm15
-			movdqu xmm4, xmm13
+			movdqu xmm3, xmm13
 			jmp .chequeoLosMayoresA255
 
 		.elNumeroEsMayorA255:
